dbee32a31de0ae9462c0d5388cde07c29f21c126
oreste-t <46974553+oreste-t@users.noreply.github.com>
2020-04-19 19:25:26 -0700

    Findopp Comments (#31)
    * Comments
    
    * Comments and optimization fix
    
    * Comments
    
    * findopp comments

--- turbo_seti/findoppler/findopp.py
+++ turbo_seti/findoppler/findopp.py
@@ -36,7 +36,9 @@ class max_vals:
         self.total_n_hits = None
 
 class hist_vals:
-    ''' Temporary class that saved the normalized spectrum for all drift rates.'''
+    """
+    Temporary class that saved the normalized spectrum for all drift rates.
+    """
     def __init__(self):
         self.histsnr = None
         self.histdrift = None
@@ -45,6 +47,20 @@ class hist_vals:
 class FinDoppler:
     """ """
     def __init__(self, datafile, max_drift, min_drift=0, snr=25.0, out_dir='./', coarse_chans=None, obs_info=None, flagging=None, n_coarse_chan=None):
+        """
+        Initializes FinDoppler object
+        :param datafile:        string,     inputted filename (.h5 or .fil)
+        :param max_drift:       float,      Max drift rate in Hz/second
+        :param min_drift:       int,        Min drift rate in Hz/second
+        :param snr:             float,      Signal to Noise Ratio - A ratio bigger than 1 to 1 has more signal than
+                                            noise
+        :param out_dir:         string,     directory where output files should be placed. By default this is the
+                                            current working directory.
+        :param coarse_chans:    list(int),  the inputted comma separated list of coarse channels to analyze, if any.
+        :param obs_info:        dict,       used to hold info found on file, including info about pulsars, RFI, and SEFD
+        :param flagging:        boolean     flags the edges of the PFF for BL data (with 3Hz res per channel)
+        :param n_coarse_chan:   int         number of coarse channels in file
+        """
         self.min_drift = min_drift
         self.max_drift = max_drift
         self.snr = snr
@@ -77,12 +93,16 @@ class FinDoppler:
         self.flagging = flagging
 
     def get_info(self):
+        """
+        :return:    string which contains the values of this FinDoppler object's attributes.
+        """
         info_str = "File: %s\n drift rates (min, max): (%f, %f)\n SNR: %f\n"%(self.data_handle.filename, self.min_drift, self.max_drift,self.snr)
         return info_str
 
     def search(self):
-        '''Top level search.
-        '''
+        """
+        Top level search.
+        """
         logger.debug("Start searching...")
         logger.debug(self.get_info())
 
@@ -102,8 +122,10 @@ class FinDoppler:
             gc.collect()
 
     def search_data(self, data_obj):
-        '''
-        '''
+        """
+        Search the waterfall data of file.
+        :param data_obj:    DATAH5,     file's waterfall data
+        """
 
         logger.info("Start searching for coarse channel: %s"%data_obj.header[b'coarse_chan'])
         self.logwriter.info("Start searching for %s ; coarse channel: %i "%(data_obj.filename,data_obj.header[b'coarse_chan']))
@@ -143,10 +165,10 @@ class FinDoppler:
         # Allocates array in a fast way (without initialize)
         tree_findoppler_original = np.empty_like(tree_findoppler)
 
-        #/* allocate array for negative doppler rates */
+        # allocate array for negative doppler rates
         tree_findoppler_flip = np.empty_like(tree_findoppler)
 
-        #/* build index mask for in-place tree doppler correction */
+        # build index mask for in-place tree doppler correction
         ibrev = np.zeros(tsteps, dtype=np.int32)
 
         for i in range(0, tsteps):
@@ -201,13 +223,13 @@ class FinDoppler:
                 #Populates the findoppler tree with the spectra
                 populate_tree(spectra,tree_findoppler,nframes,tdwidth,tsteps,fftlen,shoulder_size,roll=drift_block,reverse=1)
 
-                #/* populate original array */
+                # populate original array
                 np.copyto(tree_findoppler_original, tree_findoppler)
 
-                #/* populate neg doppler array */
+                # populate neg doppler array
                 np.copyto(tree_findoppler_flip, tree_findoppler_original)
                 
-                #/* Flip matrix across X dimension to search negative doppler drift rates */
+                # Flip matrix across X dimension to search negative doppler drift rates
                 FlipX(tree_findoppler_flip, tdwidth, tsteps)
                 logger.info("Doppler correcting reverse...")
                 tt.taylor_flt(tree_findoppler_flip, tsteps * tdwidth, tsteps)
@@ -218,10 +240,10 @@ class FinDoppler:
                     # indx  = ibrev[drift_indices[::-1][k]] * tdwidth
                     indx  = ibrev[drift_indices[::-1][(complete_drift_range<self.min_drift) & (complete_drift_range>=-1*self.max_drift)][k]] * tdwidth
 
-                    #/* SEARCH NEGATIVE DRIFT RATES */
+                    # SEARCH NEGATIVE DRIFT RATES
                     spectrum = tree_findoppler_flip[indx: indx + tdwidth]
 
-                    #/* normalize */
+                    # normalize
                     spectrum -= self.the_mean_val
                     spectrum /= self.the_stddev
 
@@ -249,7 +271,7 @@ class FinDoppler:
                 #Populates the findoppler tree with the spectra
                 populate_tree(spectra,tree_findoppler,nframes,tdwidth,tsteps,fftlen,shoulder_size,roll=drift_block,reverse=1)
 
-                #/* populate original array */
+                # populate original array
                 np.copyto(tree_findoppler_original, tree_findoppler)
 
                 logger.info("Doppler correcting forward...")
@@ -266,10 +288,10 @@ class FinDoppler:
                 for k,drift_rate in enumerate(complete_drift_range[(complete_drift_range>=self.min_drift) & (complete_drift_range<=self.max_drift)]):
 
                     indx  = ibrev[drift_indices[k]] * tdwidth
-                    #/* SEARCH POSITIVE DRIFT RATES */
+                    # SEARCH POSITIVE DRIFT RATES
                     spectrum = tree_findoppler[indx: indx+tdwidth]
 
-                    #/* normalize */
+                    # normalize
                     spectrum -= self.the_mean_val
                     spectrum /= self.the_stddev
 
@@ -300,9 +322,20 @@ class FinDoppler:
 #  ======================================================================  #
 
 def populate_tree(spectra,tree_findoppler,nframes,tdwidth,tsteps,fftlen,shoulder_size,roll=0,reverse=0):
-    """ This script populates the findoppler tree with the spectra.
-        It creates two "shoulders" (each region of tsteps*(shoulder_size/2) in size) to avoid "edge" issues.
-        It uses np.roll() for drift-rate blocks higher than 1.
+    """
+    This script populates the findoppler tree with the spectra.
+    It creates two "shoulders" (each region of tsteps*(shoulder_size/2) in size) to avoid "edge" issues.
+    It uses np.roll() for drift-rate blocks higher than 1.
+    :param spectra:             ndarray,        spectra calculated from file
+    :param tree_findoppler:     ndarray,        tree to be populated with spectra
+    :param nframes:             int,
+    :param tdwidth:             int,
+    :param tsteps:              int,
+    :param fftlen:              int,            length of fast fourier transform (fft) matrix
+    :param shoulder_size:       int,            size of shoulder region
+    :param roll:                int,            used to calculate amount each entry to the spectra should be rolled (shifted)
+    :param reverse:             int(boolean),   used to determine which way spectra should be rolled (shifted)
+    :return:                    ndarray,        spectra-populated version of the input tree_findoppler
     """
 
     if reverse:
@@ -326,18 +359,24 @@ def populate_tree(spectra,tree_findoppler,nframes,tdwidth,tsteps,fftlen,shoulder
 #EE code below will be replaced, since I think is better to use the median of the data as "flag" than to add other data into the shoulders.
 
 #         ##EE loads the end part of the current spectrum into the left hand side black region in tree_findoppler (comment below says "next spectra" but for that need i+1...bug?)
-         #//load end of current spectra into left hand side of next spectra
+         #load end of current spectra into left hand side of next spectra
         sind = i * tdwidth
         np.copyto(tree_findoppler[sind: sind + tsteps*int(shoulder_size/2)], spectra[i, fftlen-(tsteps*int(shoulder_size/2)):fftlen])
 
     return tree_findoppler
 
-#  ======================================================================  #
-#  This function bit-reverses the given value "inval" with the number of   #
-#  bits, "nbits".    ----  R. Ramachandran, 10-Nov-97, nfra.               #
-#  python version ----  H. Chen   Modified 2014                            #
-#  ======================================================================  #
+
 def bitrev(inval, nbits):
+    """
+    This function bit-reverses the given value "inval" with the number of
+    bits, "nbits".    ----  R. Ramachandran, 10-Nov-97, nfra.
+    python version ----  H. Chen   Modified 2014
+    :param inval:   number to be bit-reversed
+    :param nbits:   The length of inval in bits. If user only wants the bit-reverse of a certain amount of bits of
+                    inval, nbits is the amount of bits to be reversed counting from the least significant (rightmost)
+                    bit. Any bits beyond this length will not be reversed and will be truncated from the result.
+    :return:        the bit-reverse of inval. If there are more significant bits beyond nbits, they are truncated.
+    """
     if nbits <= 1:
         ibitr = inval
     else:
@@ -352,23 +391,35 @@ def bitrev(inval, nbits):
             ibitr += (1 & k) * ifact
     return ibitr
 
-#  ======================================================================  #
-#  This function bit-reverses the given value "inval" with the number of   #
-#  bits, "nbits".                                                          #
-#  python version ----  H. Chen   Modified 2014                            #
-#  reference: stackoverflow.com/questions/12681945                         #
-#  ======================================================================  #
+
 def bitrev2(inval, nbits, width=32):
+    """
+    This function bit-reverses the given value "inval" with the number of bits, "nbits".                                                          #
+    python version ----  H. Chen   Modified 2014
+    reference: stackoverflow.com/questions/12681945
+    This function serves the same purpose as bitrev, but is unused. It is slightly slower than bitrev. UNUSED
+    :param inval:   number to be bit-reversed
+    :param nbits:   The length of inval in bits. If user only wants the bit-reverse of a certain amount of bits of
+                    inval, nbits is the amount of bits to be reversed counting from the least significant (rightmost)
+                    bit. Any bits beyond this length will not be reversed and will be truncated from the result.
+    :param width:   the maximum length of inval in bits. Must be greater than or equal to nbits
+    :return:        the bit-reverse of inval. If there are more significant bits beyond nbits, they are truncated.
+    """
     b = '{:0{width}b}'.format(inval, width=width)
     ibitr = int(b[-1:(width-1-nbits):-1], 2)
     return ibitr
 
-#  ======================================================================  #
-#  This function bit-reverses the given value "inval" with 32bits    #
-#  python version ----  E.Enriquez   Modified 2016                            #
-#  reference: stackoverflow.com/questions/12681945                         #
-#  ======================================================================  #
+
 def bitrev3(x):
+    """
+    This function bit-reverses the given value "x" with 32bits
+    python version ----  E.Enriquez   Modified 2016
+    reference: stackoverflow.com/questions/12681945
+    Unlike the other two versions of bitrev, this one always reverses all 32 bits of the input, there is no nbits input
+    so one cannot bit-reverse only a part of the input. UNUSED
+    :param x:   32-bit number to be bit-reversed
+    :return:    bit-reversed x
+    """
     raise DeprecationWarning("WARNING: This needs testing.")
 
     x = ((x & 0x55555555) << 1) | ((x & 0xAAAAAAAA) >> 1)
@@ -379,8 +430,20 @@ def bitrev3(x):
     return x
 
 def hitsearch(spectrum, specstart, specend, hitthresh, drift_rate, header, fftlen, tdwidth, max_val, reverse):
-    ''' Searches for hits: each channel if > hitthresh.
-    '''
+    """
+    Searches for hits at given drift rate. A hit occurs in each channel if > hitthresh.
+    :param spectrum:        ndarray,
+    :param specstart:       int,                first index to search for hit in spectrum
+    :param specend:         int,                last index to search for hit in spectrum
+    :param hitthresh:       float,              signal to noise ratio used as threshold for determining hits
+    :param drift_rate:      float,              drift rate at which we are searching for hits
+    :param header:          dict,               header in fits header format. See data_handler.py's DATAH5 class header
+    :param fftlen:          int,                UNUSED
+    :param tdwidth:         int,
+    :param max_val:         max_vals,           object to be filled with max values from this search and then returned
+    :param reverse:         int(boolean),       used to flag whether fine channel should be reversed
+    :return:                int, max_vals,      j is the amount of hits.
+    """
 
     logger.debug('Start searching for hits at drift rate: %f'%drift_rate)
     j = 0
@@ -401,8 +464,25 @@ def hitsearch(spectrum, specstart, specend, hitthresh, drift_rate, header, fftle
     return j, max_val
 
 def tophitsearch(tree_findoppler_original, max_val, tsteps, nframes, header, tdwidth, fftlen,max_drift,obs_length, out_dir='', logwriter=None, filewriter=None,obs_info=None):
-    '''This finds the hits with largest SNR within 2*tsteps frequency channels.
-    '''
+    """
+    This finds the hits with largest SNR within 2*tsteps frequency channels.
+    :param tree_findoppler_original:        ndarray,        spectra-populated findoppler tree
+    :param max_val:                         max_vals,       contains max values from hitsearch
+    :param tsteps:                          int,
+    :param nframes:                         int,            UNUSED
+    :param header:                          dict,           header in fits header format. See data_handler.py's DATAH5
+                                                            class header. Used to report tophit in filewriter
+    :param tdwidth:                         int,
+    :param fftlen:                          int,            length of fast fourier transform (fft) matrix
+    :param max_drift:                       float,          Max drift rate in Hz/second
+    :param obs_length:                      float,
+    :param out_dir:                         string,         UNUSED
+    :param logwriter:                       LogWriter       logwriter to which we should write if we find a top hit
+    :param filewriter:                      FileWriter      filewriter corresponding to file to which we should save the
+                                                            local maximum of tophit. See report_tophit in filewriters.py
+    :param obs_info:                        dict,
+    :return:                                FileWriter,     same filewriter that was input
+    """
 
     maxsnr = max_val.maxsnr
     logger.debug("original matrix size: %d\t(%d, %d)"%(len(tree_findoppler_original), tsteps, tdwidth))

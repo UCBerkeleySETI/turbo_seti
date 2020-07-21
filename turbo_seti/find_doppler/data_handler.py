537bc2fdc711da7f132ce27fa2507029b64cb976
Richard Elkins <richard.elkins@gmail.com>
2020-07-21 09:05:23 -0500

    Issue #61

--- turbo_seti/find_doppler/data_handler.py
+++ turbo_seti/find_doppler/data_handler.py
@@ -1,37 +1,40 @@
-#!/usr/bin/env python
+"""
+Two classes for the Data Handler of the turbo_seti package:
+    DATAHandle
+    DATAH5
 
+Their descriptions are below.
+"""
 import os
-import numpy as np
 import math
+import logging
+
+import numpy as np
 from pkg_resources import resource_filename
-from pkg_resources import resource_filename
-from blimpy import Waterfall
-from blimpy.io import sigproc
 import h5py
 
-import logging
+from blimpy import Waterfall
+from blimpy.io import sigproc
+
 logger = logging.getLogger(__name__)
 
 #For debugging
 #import cProfile
 #import pdb;# pdb.set_trace()
 
-SIZE_LIM = 256.0   # File size limit in MB. If larger then make a split mapping.
-
 class DATAHandle:
-    """Class to setup input file for further processing of data. Handles conversion to h5 (from fil), extraction of
-    coarse channel info, waterfall info, and file size checking.
-
-    Args:
-
-    Returns:
-
     """
-    def __init__(self, filename=None, size_limit=SIZE_LIM, out_dir='./', n_coarse_chan=None, coarse_chans=None):
+    Class to setup input file for further processing of data.
+    Handles conversion to h5 (from fil), extraction of
+    coarse channel info, waterfall info, and file size checking.
+    """
+
+    def __init__(self, filename=None, out_dir='./', n_coarse_chan=None, coarse_chans=None):
         """
         :param filename:        string,      name of file (.h5 or .fil)
-        :param size_limit:      float,       maximum size in MB that the file is allowed to be
         :param out_dir:         string,      directory where output files should be saved
+        :param n_coarse_chan:   integer,     number of coarse channels
+        :param coarse_chans     list or None,   list of course channels
         """
 
         if filename and os.path.isfile(filename):
@@ -42,13 +45,12 @@ class DATAHandle:
 
             if not h5py.is_hdf5(filename):
                 if not sigproc.is_filterbank(filename):
-                    raise IOError('No correct format, need .h5. Try again...')
-                else:
-                    logger.info("File .fil detected. Attempting to create .h5 file in current directory...")
-                    try:
-                        self.__make_h5_file()
-                    except:
-                        raise IOError('Unable to create .h5 file. Please, try again with correct format.')
+                    raise IOError('Not a filterbank file: {}'.format(filename))
+                logger.info("Filterbank file detected. Attempting to create .h5 file in current directory...")
+                try:
+                    self.__make_h5_file()
+                except:
+                    raise IOError('Unable to create .h5 file from: {}.format(filename)')
 
             self.filestat = os.stat(filename)
             self.filesize = self.filestat.st_size/(1024.0**2)
@@ -58,14 +60,8 @@ class DATAHandle:
             self.header = dobj_master.header
             dobj_master.close()
 
-            # Make sure file is not larger than limit. If it is, we must split the file
+            # Split the file
             self.data_list = self.__split_h5()
-            #else:
-            #    #EE This is here mainly for testing. Since need to keep in mind the band pass shape.
-            #    logger.debug("File size %f MB within range %f MB, okay..."%(self.filesize, size_limit))
-            #    data_obj = DATAH5(filename)
-            #    self.data_list = [data_obj]
-
             self.status = True
 
         else:
@@ -77,12 +73,13 @@ class DATAHandle:
     def get_info(self):
         """:return:    dict,    header of the blimpy file"""
 
-        fil_file = Waterfall(self.filename,load_data=False)
+        fil_file = Waterfall(self.filename, load_data=False)
         return fil_file.header
 
-    def __make_h5_file(self,):
+    def __make_h5_file(self):
         """
-        Converts file to h5 format. Saves output in current dir. Sets the filename attribute of the calling DATAHandle
+        Converts file to h5 format, saved in current directory.
+        Sets the filename attribute of the calling DATAHandle
         to the (new) filename.
         :return: void
         """
@@ -93,12 +90,12 @@ class DATAHandle:
         fil_file.write_to_hdf5(new_filename)
         self.filename = new_filename
 
-    def __split_h5(self, size_limit=SIZE_LIM):
+    def __split_h5(self):
         """
         Creates a plan to select data from single coarse channels.
-        :param size_limit:      float,              maximum size in MB that the file is allowed to be
-        :return:                list[DATAH5],       the list contains a DATAH5 object for each of the coarse channels in
-                                                    the file
+        :return: list[DATAH5],
+            where each list member contains a DATAH5 object
+            for each of the coarse channels in the file.
         """
 
         data_list = []
@@ -107,8 +104,9 @@ class DATAHandle:
         try:
             fil_file = Waterfall(self.filename)
         except:
-            logger.error("Error encountered when trying to open file: %s"%self.filename)
-            raise IOError("Error encountered when trying to open file: %s"%self.filename)
+            errmsg = "Error encountered when trying to open file: {}".format(self.filename)
+            logger.error(errmsg)
+            raise IOError(errmsg)
 
         #Finding lowest freq in file.
         f_delt = fil_file.header['foff']
@@ -129,8 +127,8 @@ class DATAHandle:
         for chan in self.coarse_chans:
 
             #Calculate freq range for given course channel.
-            f_start = f0 + chan*(f_delt)*fil_file.n_channels_in_file/n_coarse_chan
-            f_stop = f0 + (chan+1)*(f_delt)*fil_file.n_channels_in_file/n_coarse_chan
+            f_start = f0 + chan * (f_delt) * fil_file.n_channels_in_file / n_coarse_chan
+            f_stop = f0 + (chan + 1) * (f_delt) * fil_file.n_channels_in_file / n_coarse_chan
 
             if f_start > f_stop:
                 f_start, f_stop = f_stop, f_start
@@ -147,20 +145,15 @@ class DATAHandle:
         return data_list
 
 class DATAH5:
-    """This class is where the waterfall data is loaded, as well as the header info.
+    """
+    This class is where the waterfall data is loaded, as well as the header info.
     It creates other attributes related to the dedoppler search (load_drift_indexes).
-
-    Args:
-
-    Returns:
-
     """
 
-    def __init__(self, filename, size_limit=SIZE_LIM, f_start=None, f_stop=None, t_start=None, t_stop=None,
+    def __init__(self, filename, f_start=None, f_stop=None, t_start=None, t_stop=None,
                  coarse_chan=1, tn_coarse_chan=None):
         """
         :param filename:        string      name of file
-        :param size_limit:      float       maximum size in MB that the file is allowed to be
         :param f_start:         float       start frequency in MHz
         :param f_stop:          float       stop frequency in MHz
         :param t_start:         int         start integration ID
@@ -179,42 +172,46 @@ class DATAH5:
 
         #Instancing file.
         try:
-            self.fil_file = Waterfall(filename,f_start=self.f_start, f_stop=self.f_stop,t_start=self.t_start, t_stop=self.t_stop,load_data=False)
+            self.fil_file = Waterfall(filename, f_start=self.f_start, f_stop=self.f_stop,
+                                      t_start=self.t_start, t_stop=self.t_stop, load_data=False)
         except:
-            logger.error("Error encountered when trying to open file %s"%filename)
-            raise IOError("Error encountered when trying to open file %s"%filename)
+            errmsg = "Error encountered when trying to open file: {}".format(filename)
+            logger.error(errmsg)
+            raise IOError(errmsg)
 
         #Getting header
         try:
             if self.tn_coarse_chan:
-                header = self.__make_data_header(self.fil_file.header,coarse=True)
+                header = self.__make_data_header(self.fil_file.header, coarse=True)
             else:
                 header = self.__make_data_header(self.fil_file.header)
         except:
-            logger.debug('The fil_file.header is ' % self.fil_file.header)
-            raise IOError("Error accessing header from file: %s." % self.filename)
+            errmsg = "Error accessing header from file: {}".format(self.fil_file.header)
+            logger.error(errmsg)
+            raise IOError(errmsg)
 
         self.header = header
 
         self.fftlen = header['NAXIS1']
- 
+
         #EE To check if swapping tsteps_valid and tsteps is more appropriate.
         self.tsteps_valid = header['NAXIS2']
         self.tsteps = int(math.pow(2, math.ceil(np.log2(math.floor(self.tsteps_valid)))))
- 
+
         self.obs_length = self.tsteps_valid * header['DELTAT']
         self.drift_rate_resolution = (1e6 * np.abs(header['DELTAF'])) / self.obs_length   # in Hz/sec
         self.header['baryv'] = 0.0
         self.header['barya'] = 0.0
         self.header['coarse_chan'] = coarse_chan
-        
-        #EE For now I'm not using a shoulder. This is ok as long as I'm analyzing each coarse channel individually.
+
+        #EE For now I'm not using a shoulder. This is ok as long as
+        ##  I'm analyzing each coarse channel individually.
         #EE In general this parameter is an integer (even number).
         #This gives two regions, each of n*steps, around spectra[i]
         self.shoulder_size = 0
-        self.tdwidth = self.fftlen + self.shoulder_size*self.tsteps
+        self.tdwidth = self.fftlen + self.shoulder_size * self.tsteps
 
-    def load_data(self,):
+    def load_data(self):
         """Read the spectra and drift indices from file.
         :return:    ndarray, ndarray        spectra, drift indices
 
@@ -239,9 +236,11 @@ class DATAH5:
         #if self.header['DELTAF'] < 0.0:
         #    spectra = spectra[:,::-1]
 
-        #This check will add rows of zeros if the obs is too short (and thus not a power of two rows).
+        # This check will add rows of zeros if the obs is too short
+        # (and thus not a power of two rows).
         if spectra.shape[0] != self.tsteps:
-            spectra = np.append(spectra, np.zeros((self.tsteps-spectra.shape[0], self.fftlen)), axis=0)
+            spectra = np.append(spectra, np.zeros((self.tsteps-spectra.shape[0],
+                                                   self.fftlen)), axis=0)
         self.tsteps_valid = self.tsteps
         self.obs_length = self.tsteps * self.header['DELTAT']
 
@@ -254,7 +253,9 @@ class DATAH5:
         return spectra, drift_indexes
 
     def load_drift_indexes(self):
-        """The drift indices are read from a stored file so that there is no need to recalculate. This speed things up.
+        """
+        The drift indices are read from a stored file so that
+        there is no need to recalculate. This speed things up.
         :return:    ndarray     drift indices
 
         Args:
@@ -263,13 +264,15 @@ class DATAH5:
 
         """
         n = int(np.log2(self.tsteps))
-        di_array = np.genfromtxt(resource_filename('turbo_seti', 'drift_indexes/drift_indexes_array_%d.txt'%n), delimiter=' ', dtype=int)
+        di_array = np.genfromtxt(resource_filename('turbo_seti',
+                                                   'drift_indexes/drift_indexes_array_%d.txt'%n),
+                                 delimiter=' ', dtype=int)
 
         ts2 = int(self.tsteps/2)
         drift_indexes = di_array[(self.tsteps_valid - 1 - ts2), 0:self.tsteps_valid]
         return drift_indexes
 
-    def __make_data_header(self,header,coarse=False):
+    def __make_data_header(self, header, coarse=False):
         """
         Takes header into fits header format
         :param header:      dict        blimpy waterfall header
@@ -284,13 +287,13 @@ class DATAH5:
         base_header['MJD'] = header['tstart']
         base_header['DEC'] = str(header['src_dej'])
         base_header['RA'] = str(header['src_raj'])
-        base_header['DELTAF'] =  header['foff']
+        base_header['DELTAF'] = header['foff']
         base_header['DELTAT'] = float(header['tsamp'])
 
         #used by helper_functions.py
         if coarse:
             base_header['NAXIS1'] = int(header['nchans']/self.tn_coarse_chan)
-            base_header['FCNTR'] = np.abs(self.f_stop - self.f_start)/2. + np.fmin(self.f_start, self.f_stop)
+            base_header['FCNTR'] = np.abs(self.f_stop - self.f_start) / 2. + np.fmin(self.f_start, self.f_stop)
         else:
             base_header['NAXIS1'] = int(header['nchans'])
             base_header['FCNTR'] = float(header['fch1']) + header['foff'] * base_header['NAXIS1'] / 2
@@ -315,4 +318,3 @@ class DATAH5:
         del self.fil_file
 
         self.closed = True
-

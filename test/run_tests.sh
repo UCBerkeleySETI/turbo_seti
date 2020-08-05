fe335813d50f92e9473d0fcc16ba968c5a2323a4
Richard Elkins <richard.elkins@gmail.com>
2020-08-05 09:24:46 -0500

    Issue #90

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,3 +1,17 @@
 coverage run --source=turbo_seti -m pytest
+EXITCODE=$?
+if [ $EXITCODE -ne 0 ]; then
+    echo
+    echo '*** Oops, coverage pytest failed, exit code = '$EXITCODE' ***'
+    echo
+    exit $EXITCODE
+fi
 coverage report
+EXITCODE=$?
+if [ $EXITCODE -ne 0 ]; then
+    echo
+    echo '*** Oops, coverage report failed, exit code = '$EXITCODE' ***'
+    echo
+    exit $EXITCODE
+fi
 codecov

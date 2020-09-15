9df329e113b02b9df01e5d419d07683a0b552c51
Danny Price <dan@thetelegraphic.com>
2020-09-15 16:59:19 +0800

    Minor change

--- test/test_pipelines.py
+++ test/test_pipelines.py
@@ -205,4 +205,4 @@ def test_pipelines(need_init=True, cleanup=True):
 
 if __name__ == '__main__':
     # When run manually, no initialization nor cleanup is performed.
-    test_pipelines(need_init=False, cleanup=False)
+    test_pipelines(need_init=True, cleanup=False)

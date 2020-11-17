cc2113efe4598c2359831cf9be2f4e0adf9b42fd
Luigi Cruz <luigifcruz@gmail.com>
2020-11-17 16:00:41 -0300

    bump version to 2.0.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 import numpy
 from setuptools.extension import Extension
 
-__version__ = "1.3.3"
+__version__ = "2.0.0"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()

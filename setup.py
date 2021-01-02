f1d6109ab865062bafd24d391f89d906bfe55104
Richard Elkins <richard.elkins@gmail.com>
2021-01-02 15:01:44 -0600

    Bump version -> 2.0.4.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 import numpy
 from setuptools.extension import Extension
 
-__version__ = "2.0.4.1"
+__version__ = "2.0.4.2"
 
 with open("README.md", "r") as fh:
     long_description = fh.read()

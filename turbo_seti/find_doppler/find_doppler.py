7a758264bbd48147b8575ca7298b7cbf1f20ec43
Richard Elkins <richard.elkins@gmail.com>
2021-01-13 13:52:43 -0600

    Use '-' instead of '_' in turbo_seti

--- turbo_seti/find_doppler/find_doppler.py
+++ turbo_seti/find_doppler/find_doppler.py
@@ -18,7 +18,7 @@ try:
 except ImportError:
     # Running on pre-3.8 Python; use importlib-metadata package
     import importlib_metadata as metadata
-VERSION = metadata.version('turbo_seti')
+VERSION = metadata.version('turbo-seti')
 
 #For debugging
 #import pdb;# pdb.set_trace()

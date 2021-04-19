c409e0eb28350905936f3f5f1128fe5ec34238aa
Richard Elkins <richard.elkins@gmail.com>
2021-04-19 14:56:16 -0500

    Create plots subdirectory if not already in existence

--- test/test_plot_dat.py
+++ test/test_plot_dat.py
@@ -2,19 +2,25 @@ r'''test_plot_dat - test plotting of a DAT file.
 NOTE:  This source file uses data downloaded by test_pipelines_1.py
 '''
 
+from shutil import rmtree
+from os import mkdir
 import glob
 from tempfile import gettempdir
 from turbo_seti.find_event import plot_dat
 
-TESTDIR = gettempdir() + '/pipeline_testing/'
-PLOTDIR = TESTDIR + 'plots/'
-h5_list = sorted(glob.glob(TESTDIR + 'single*.h5'))
-dat_list = sorted(glob.glob(TESTDIR + 'single*.dat'))
-H5_LIST_FILE  = TESTDIR + 'h5_files.lst'
-DAT_LIST_FILE = TESTDIR + 'dat_files.lst'
-PATH_CSVF = TESTDIR + 'found_event_table.csv'
+TEMPDIR = gettempdir() + '/pipeline_testing/'
+PLOTDIR = TEMPDIR + 'plots/'
+h5_list = sorted(glob.glob(TEMPDIR + 'single*.h5'))
+dat_list = sorted(glob.glob(TEMPDIR + 'single*.dat'))
+H5_LIST_FILE  = TEMPDIR + 'h5_files.lst'
+DAT_LIST_FILE = TEMPDIR + 'dat_files.lst'
+PATH_CSVF = TEMPDIR + 'found_event_table.csv'
 
 def test_plot_dat():
+
+    rmtree(PLOTDIR, ignore_errors=True)
+    mkdir(PLOTDIR)
+
     # test default settings, will produce six plots, all candidates
     plot_dat.plot_dat(DAT_LIST_FILE, H5_LIST_FILE, PATH_CSVF)
 

0dcf647c6f251ae35eaaf370ab694e0ea981d359
savinshynu <47194831+savinshynu@users.noreply.github.com>
2022-02-23 10:23:22 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-__version__ = "2.1.24"
+__version__ = "2.1.25"
 
 with open("turbo_seti/find_doppler/turbo_seti_version.py", "w") as fh:
     fh.write("TURBO_SETI_VERSION = '{}'\n".format(__version__))
